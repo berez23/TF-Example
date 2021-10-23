cec01947c076dcd9e309cc5a017002ca0318c2da
Andrea Ceresoni <andrea.ceresoni@gmail.com>
2021-10-23 17:35:01 +0200

    create vars.tf

---
+++ vars.tf
@@ -0,0 +1,3 @@
+variable "public_key"               {default = "your_public_key"}
+variable "shared_credentials_file"  { default = "~/.aws/credentials" }
+variable "profile"                  { default = "demonstration" }
